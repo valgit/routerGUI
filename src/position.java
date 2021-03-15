0925e1262529af890bd14e0981b135cc61b6d916
fr20033 <valery.brasseur@worldline.net>
2021-03-15 15:18:39 +0100

    initial commit

---
+++ src/position.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class position {
+    private JTextField longitude;
+    private JTextField latitude;
+    private JTextField LatArr;
+    private JTextField textField1;
+    private JButton calculButton;
+}
